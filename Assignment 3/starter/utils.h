3a28b8f5dd31ade8b8be970e88f28be40d18550e
Sandro Young <ord-nas@users.noreply.github.com>
2017-04-07 01:14:07 -0400

    Reindent everything.

--- Assignment 3/starter/utils.h
+++ Assignment 3/starter/utils.h
@@ -23,113 +23,105 @@
 
 // Functions to apply transformations to objects.
 // If you add any transformations to the list below, document them carefully
-inline void matMult(double A[4][4], double B[4][4])
-{
- // Performs matrix multiplication B=A*B (notice the result is left
- // in B). This is so that we can compose transformations by
- // premultiplying a given transformation matrix by one of our
- // simple transformation matrices (rotate, translate, scale, etc).
- // Note the indexing convention is [row][col]
+inline void matMult(double A[4][4], double B[4][4]) {
+  // Performs matrix multiplication B=A*B (notice the result is left
+  // in B). This is so that we can compose transformations by
+  // premultiplying a given transformation matrix by one of our
+  // simple transformation matrices (rotate, translate, scale, etc).
+  // Note the indexing convention is [row][col]
 
- double C[4][4];
- int i,j,k;
+  double C[4][4];
+  int i,j,k;
 
- memset(C,0,16*sizeof(double));
- for (i=0;i<4;i++)
-  for (j=0;j<4;j++)
-   C[i][j]=(A[i][0]*B[0][j])+(A[i][1]*B[1][j])+(A[i][2]*B[2][j])+(A[i][3]*B[3][j]);
+  memset(C,0,16*sizeof(double));
+  for (i=0;i<4;i++)
+    for (j=0;j<4;j++)
+      C[i][j]=(A[i][0]*B[0][j])+(A[i][1]*B[1][j])+(A[i][2]*B[2][j])+(A[i][3]*B[3][j]);
 
- memcpy(B,C,16*sizeof(double));
+  memcpy(B,C,16*sizeof(double));
 }
 
-inline void matVecMult(double A[4][4], struct point3D *pt)
-{
- // Matrix vector multiplication pt=A*pt, notice that the result
- // is left in pt. This is useful for performing transformations
- // on points and rays
- struct point3D pr;
+inline void matVecMult(double A[4][4], struct point3D *pt) {
+  // Matrix vector multiplication pt=A*pt, notice that the result
+  // is left in pt. This is useful for performing transformations
+  // on points and rays
+  struct point3D pr;
 
- pr.px=(A[0][0]*pt->px)+(A[0][1]*pt->py)+(A[0][2]*pt->pz)+(A[0][3]*pt->pw);
- pr.py=(A[1][0]*pt->px)+(A[1][1]*pt->py)+(A[1][2]*pt->pz)+(A[1][3]*pt->pw);
- pr.pz=(A[2][0]*pt->px)+(A[2][1]*pt->py)+(A[2][2]*pt->pz)+(A[2][3]*pt->pw);
- pr.pw=(A[3][0]*pt->px)+(A[3][1]*pt->py)+(A[3][2]*pt->pz)+(A[3][3]*pt->pw);
- memcpy(pt,&pr,4*sizeof(double));
+  pr.px=(A[0][0]*pt->px)+(A[0][1]*pt->py)+(A[0][2]*pt->pz)+(A[0][3]*pt->pw);
+  pr.py=(A[1][0]*pt->px)+(A[1][1]*pt->py)+(A[1][2]*pt->pz)+(A[1][3]*pt->pw);
+  pr.pz=(A[2][0]*pt->px)+(A[2][1]*pt->py)+(A[2][2]*pt->pz)+(A[2][3]*pt->pw);
+  pr.pw=(A[3][0]*pt->px)+(A[3][1]*pt->py)+(A[3][2]*pt->pz)+(A[3][3]*pt->pw);
+  memcpy(pt,&pr,4*sizeof(double));
 }
 
 void invert(double *T, double *Tinv);
-void RotateX(struct object3D *o, double theta);	// Rotate theta radians CCW around X axis
-void RotateY(struct object3D *o, double theta);	// Rotate theta radians CCW around Y axis
-void RotateZ(struct object3D *o, double theta);	// Rotate theta radians CCW around Z axis
-void Translate(struct object3D *o, double tx, double ty, double tz);	// 3D translation
-void Scale(struct object3D *o, double sx, double sy, double sz);	// Non-uniform scaling
+void RotateX(struct object3D *o, double theta); // Rotate theta radians CCW around X axis
+void RotateY(struct object3D *o, double theta); // Rotate theta radians CCW around Y axis
+void RotateZ(struct object3D *o, double theta); // Rotate theta radians CCW around Z axis
+void Translate(struct object3D *o, double tx, double ty, double tz);    // 3D translation
+void Scale(struct object3D *o, double sx, double sy, double sz);        // Non-uniform scaling
 void printmatrix(double mat[4][4]);
 
 // Vector management
-inline void normalize(struct point3D *v)
-{
- // Normalizes a vector to unit length.
- // Note that this assumes the w component of v is one, so make
- // sure you don't have homogeneous vectors with w != 1
- // floating around or things will go south.
- double l;
- l=v->px*v->px;
- l+=(v->py*v->py);
- l+=(v->pz*v->pz);
- l=1.0/sqrt(l);
- v->px*=l;
- v->py*=l;
- v->pz*=l;
+inline void normalize(struct point3D *v) {
+  // Normalizes a vector to unit length.
+  // Note that this assumes the w component of v is one, so make
+  // sure you don't have homogeneous vectors with w != 1
+  // floating around or things will go south.
+  double l;
+  l=v->px*v->px;
+  l+=(v->py*v->py);
+  l+=(v->pz*v->pz);
+  l=1.0/sqrt(l);
+  v->px*=l;
+  v->py*=l;
+  v->pz*=l;
 }
 
-inline double dot(struct point3D *u, struct point3D *v)
-{
- // Computes the dot product of 3D vectors u and v.
- // The function assumes the w components of both vectors
- // are 1.
- return((u->px*v->px)+(u->py*v->py)+(u->pz*v->pz));
+inline double dot(struct point3D *u, struct point3D *v) {
+  // Computes the dot product of 3D vectors u and v.
+  // The function assumes the w components of both vectors
+  // are 1.
+  return((u->px*v->px)+(u->py*v->py)+(u->pz*v->pz));
 }
 
-inline struct point3D *cross(struct point3D *u, struct point3D *v)
-{
- // Allocates and returns a vector with the cross product u x v.
- // The function assumes the w components of both vectors
- // are 1.
- struct point3D *cp;
- cp=(struct point3D *)calloc(1,sizeof(struct point3D));
+inline struct point3D *cross(struct point3D *u, struct point3D *v) {
+  // Allocates and returns a vector with the cross product u x v.
+  // The function assumes the w components of both vectors
+  // are 1.
+  struct point3D *cp;
+  cp=(struct point3D *)calloc(1,sizeof(struct point3D));
 
- cp->px=(u->py*v->pz)-(v->py*u->pz);
- cp->py=(v->px*u->pz)-(u->px*v->pz);
- cp->pz=(u->px*v->py)-(v->px*u->py);
- cp->pw=1;
- return(cp);
+  cp->px=(u->py*v->pz)-(v->py*u->pz);
+  cp->py=(v->px*u->pz)-(u->px*v->pz);
+  cp->pz=(u->px*v->py)-(v->px*u->py);
+  cp->pw=1;
+  return(cp);
 }
 
-inline void addVectors(struct point3D *a, struct point3D *b)
-{
- // Performs the vector addition b=a+b. Note the result
- // is left in b. This function assumes the w components
- // of both vectors are set to 1.
- b->px=b->px+a->px;
- b->py=b->py+a->py;
- b->pz=b->pz+a->pz;
- b->pw=1;               // Mind the homogeneous coordinate!
+inline void addVectors(struct point3D *a, struct point3D *b) {
+  // Performs the vector addition b=a+b. Note the result
+  // is left in b. This function assumes the w components
+  // of both vectors are set to 1.
+  b->px=b->px+a->px;
+  b->py=b->py+a->py;
+  b->pz=b->pz+a->pz;
+  b->pw=1;               // Mind the homogeneous coordinate!
 }
 
-inline void subVectors(struct point3D *a, struct point3D *b)
-{
- // Performs the vector subtraction b=b-a. Note the result
- // is left in b. This function assumes the w components
- // of both vectors are set to 1.
- b->px=b->px-a->px;
- b->py=b->py-a->py;
- b->pz=b->pz-a->pz;
- b->pw=1;               // Mind the homogeneous coordinate!
+inline void subVectors(struct point3D *a, struct point3D *b) {
+  // Performs the vector subtraction b=b-a. Note the result
+  // is left in b. This function assumes the w components
+  // of both vectors are set to 1.
+  b->px=b->px-a->px;
+  b->py=b->py-a->py;
+  b->pz=b->pz-a->pz;
+  b->pw=1;               // Mind the homogeneous coordinate!
 }
 
-inline double length(struct point3D *a)
-{
- // Compute and return the length of a vector
- return(sqrt((a->px*a->px)+(a->py*a->py)+(a->pz*a->pz)));
+inline double length(struct point3D *a) {
+  // Compute and return the length of a vector
+  return(sqrt((a->px*a->px)+(a->py*a->py)+(a->pz*a->pz)));
 }
 
 // Functions to instantiate primitives
@@ -137,31 +129,28 @@ struct point3D *newPoint(double px, double py, double pz);
 struct pointLS *newPLS(struct point3D *p0, double r, double g, double b);
 
 // Ray management inlines
-inline void rayPosition(struct ray3D *ray, double lambda, struct point3D *pos)
-{
- // Compute and return 3D position corresponding to a given lambda
- // for the ray.
- pos->px=ray->p0.px+(lambda*ray->d.px);
- pos->py=ray->p0.py+(lambda*ray->d.py);
- pos->pz=ray->p0.pz+(lambda*ray->d.pz);
- pos->pw=1;
+inline void rayPosition(struct ray3D *ray, double lambda, struct point3D *pos) {
+  // Compute and return 3D position corresponding to a given lambda
+  // for the ray.
+  pos->px=ray->p0.px+(lambda*ray->d.px);
+  pos->py=ray->p0.py+(lambda*ray->d.py);
+  pos->pz=ray->p0.pz+(lambda*ray->d.pz);
+  pos->pw=1;
 }
 
-inline struct ray3D *newRay(struct point3D *p0, struct point3D *d)
-{
- // Allocate a new ray structure and initialize it to the values
- // given by p0 and d. Note that this function DOES NOT normalize
- // d to be a unit vector.
+inline struct ray3D *newRay(struct point3D *p0, struct point3D *d) {
+  // Allocate a new ray structure and initialize it to the values
+  // given by p0 and d. Note that this function DOES NOT normalize
+  // d to be a unit vector.
 
- struct ray3D *ray=(struct ray3D *)calloc(1,sizeof(struct ray3D));
- if (!ray) fprintf(stderr,"Out of memory allocating ray structure!\n");
- else
- {
-  memcpy(&ray->p0,p0,sizeof(struct point3D));
-  memcpy(&ray->d,d,sizeof(struct point3D));
-  ray->rayPos=&rayPosition;
- }
- return(ray);
+  struct ray3D *ray=(struct ray3D *)calloc(1,sizeof(struct ray3D));
+  if (!ray) fprintf(stderr,"Out of memory allocating ray structure!\n");
+  else {
+    memcpy(&ray->p0,p0,sizeof(struct point3D));
+    memcpy(&ray->d,d,sizeof(struct point3D));
+    ray->rayPos=&rayPosition;
+  }
+  return(ray);
 }
 
 // Ray and normal transformations to enable the use of canonical intersection tests with transformed objects
